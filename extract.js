af71d4b281857972f0f6393050e150b15f840427
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-07-31 10:53:57 -0400

    Create extract.js

---
+++ extract.js
@@ -0,0 +1,29 @@
+var apiLib = {
+    get: function (site,callback) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                callback(this.responseText);
+            }
+        };
+        xhttp.open('GET', site, true);
+        xhttp.send();
+    }
+};
+apiLib.get("https://amazinigmech2418.github.io/ObjectifyJS/objects.js",function(data){eval(data);});
+apiLib.get("https://amazinigmech2418.github.io/ObjectifyJS/numbers.js",function(data){eval(data);});
+apiLib.get("https://amazinigmech2418.github.io/ObjectifyJS/strings.js",function(data){eval(data);});
+function extract(arg) {
+var numberMap = arg.toNumberMap();
+for(var zzz=0; zzz<numberMap.length; zzz++) {
+numberMap[zzz] = numberMap[zzz].toString(2);
+}
+
+}
+var array = [];
+for (var z = 0; z<numberMap.length; z++) {
+array.push(String.fromCharCode(parseInt(numberMap[z].slice(0,7),2)));
+array.push(String.fromCharCode(parseInt(numberMap[z].slice(7),2)));
+}
+return array.join("");
+}
