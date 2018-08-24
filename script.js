d4abaac2ed30bf606039bf3d57bb8202d3a63c6b
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-08-24 18:23:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -35,10 +35,11 @@ function extract(arg) {
 var numberMap = arg.toNumberMap();
 for(var zzz=0; zzz<numberMap.length; zzz++) {
 numberMap[zzz] = numberMap[zzz].toString(2);
+    while(numberMap[zzz].length < 8) {
+numberMap[zzz] = "0" + numberMap[zzz];
 }
-while(numberMap[zz].length < 8) {
-numberMap[zz] = "0" + numberMap[zz];
 }
+
 
 var array = [];
 for (var z = 0; z<numberMap.length; z++) {
